86074c3b2c9b076adc513e245d149dde03d15599
Joe Bowbeer <joe.bowbeer@gmail.com>
2017-04-07 15:20:54 -0700

    Initial create

---
+++ src/main/java/com/joebowbeer/linkedpalindrome/Main.java
@@ -0,0 +1,7 @@
+package com.joebowbeer.linkedpalindrome;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println(Node.fromString("oso").isPalindrome());
+    }
+}
